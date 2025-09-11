5d5bbcdf70ade95128d01999d3898420db13865f
vadimpopov1 <bonia07052006@icloud.com>
2025-09-11 13:00:48 +0700

    Update Main.kt

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -3,8 +3,9 @@ class Human
     var name: String = ""
     var surname: String = ""
     var second_name: String = ""
-
     var group_number: Int = -1
+    var x = 0
+    var y = 0
 
     constructor(_name: String, _surname: String, _second_name: String, _group_name: Int ) {
         name = _name
@@ -17,15 +18,19 @@ class Human
     fun move() {
         println("Human in moved")
     }
+
+    fun moveTo(_toX: Int, _toY: Int) {
+        x  = _toX
+        y = _toY
+        println("Human is moved TO: $x, $y")
+    }
 }
 
 fun main() {
 
     val petya: Human = Human("Petya", "Ivanov", "Petrovich", 444)
     petya.move()
-
-    val counter: Int = 10
-    val name: String = ""
-    println(name)
+    petya.moveTo(10, 100)
+    println("${petya.name}")
 
 }
