afb195af56347c68e40733ea2f41774109f9674a
vadimpopov1 <bonia07052006@icloud.com>
2025-09-14 18:46:08 +0700

    changed Main.kt

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -1,36 +1,90 @@
+import java.util.Random
+
 class Human
 {
     var name: String = ""
     var surname: String = ""
     var second_name: String = ""
+    var age: Int = 0
     var group_number: Int = -1
+    var cur_speed: Int = 1
     var x = 0
     var y = 0
 
-    constructor(_name: String, _surname: String, _second_name: String, _group_name: Int ) {
+    constructor(_name: String, _surname: String, _second_name: String, _age: Int, _cur_speed: Int, _group_name: Int ) {
         name = _name
         surname = _surname
         second_name = _second_name
+        age = _age
+        cur_speed = _cur_speed
         group_number = _group_name
         println("We created the Human object with name: $name")
     }
 
-    fun move() {
-        println("Human in moved")
+    fun moveTo(_toX: Int, _toY: Int) {
+        x  += _toX * cur_speed
+        y += _toY * cur_speed
+        println("Human $name with speed $cur_speed moved on $_toX by x and $_toY by y. Current position: $x, $y")
     }
 
-    fun moveTo(_toX: Int, _toY: Int) {
-        x  = _toX
-        y = _toY
-        println("Human is moved TO: $x, $y")
+    fun printFIO() {
+        println("$surname $name $second_name, age: $age, group: $group_number")
     }
 }
 
 fun main() {
+    val random = Random()
 
-    val petya: Human = Human("Petya", "Ivanov", "Petrovich", 444)
-    petya.move()
-    petya.moveTo(10, 100)
-    println("${petya.name}")
+    //val petya: Human = Human("Petya", "Ivanov", "Petrovich", 444)
 
+    val humans = arrayOf(
+        Human("Алексей", "Иванов", "Сергеевич", 23, 1, 444),
+        Human("Дмитрий", "Петров", "Александрович", 24, 1, 444),
+        Human("Михаил", "Федоров", "Дмитриевич", 18,1, 444),
+        Human("Иван", "Смирнов", "Игоревич", 19, 2,444),
+        Human("Артем", "Кузнецов", "Олегович", 20,2, 444),
+        Human("Сергей", "Попов", "Викторович", 31, 2,444),
+        Human("Андрей", "Васильев", "Николаевич", 19,3, 444),
+        Human("Павел", "Павлов", "Андреевич", 19, 6,555),
+        Human("Максим", "Голубев", "Артемович", 20,5, 555),
+        Human("Никита", "Воробьев", "Максимович", 22, 7,555),
+        Human("Кирилл", "Титов", "Павлович", 28, 8,555),
+        Human("Егор", "Королев", "Кириллович", 29,1, 555),
+        Human("Константин", "Белов", "Егорович", 26, 2,555),
+        Human("Юрий", "Медведев", "Константинович",23,3, 555),
+        Human("Владимир", "Антонов", "Романович", 22,4, 666),
+        Human("Олег", "Тихонов", "Владимирович", 23, 5,666),
+        Human("Глеб", "Федоров", "Олегович", 25, 6,666),
+        Human("Юрий", "Щербаков", "Глебович", 22,7, 666),
+        Human("Виктор", "Борисов", "Юрьевич", 29,8, 666),
+        Human("Станислав", "Орлов", "Викторович", 31,9, 666),
+        Human("Тимофей", "Комаров", "Станиславович", 19,10,666)
+    )
+
+    for (person in humans)
+    {
+        person.printFIO()
+    }
+
+    println("\nEnter count of seconds. (While this time humans will walk!)")
+    val input = readln()
+    val seconds = try {
+        input.toInt().coerceAtLeast(0)
+    } catch (e: NumberFormatException) {
+        println("Invalid input. Use Int value or not least 1! \n Using default value = 1.")
+        1
+    }
+
+    for (person in humans) {
+        for (i in 1..seconds) {
+
+            val rand_x = random.nextInt(-1, 1)
+            val rand_y = random.nextInt(-1, 1)
+            person.moveTo(rand_x, rand_y)
+            Thread.sleep(1000)
+        }
+        println()
+    }
+
+    //petya.moveTo(rand_x, rand_y)
 }
